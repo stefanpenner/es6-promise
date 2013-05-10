8c11dcc8acfef684077777c27f8760bbbc584049
Stefan Penner <stefan.penner@gmail.com>
2013-05-10 01:52:45 -0500

    refactor RSVP.all to just use a promise directly

--- browser/rsvp.js
+++ browser/rsvp.js
@@ -32,44 +32,42 @@ var define, requireModule;
 })();
 
 define("rsvp/all",
-  ["rsvp/defer","exports"],
+  ["rsvp/promise","exports"],
   function(__dependency1__, __exports__) {
     "use strict";
-    var defer = __dependency1__.defer;
+    var Promise = __dependency1__.Promise;
 
     function all(promises) {
-      var results = [], deferred = defer(), remaining = promises.length;
+      return new Promise(function(resolve, reject) {
+        var results = [], remaining = promises.length;
 
-      if (remaining === 0) {
-        deferred.resolve([]);
-      }
-
-      var resolver = function(index) {
-        return function(value) {
-          resolveAll(index, value);
-        };
-      };
-
-      var resolveAll = function(index, value) {
-        results[index] = value;
-        if (--remaining === 0) {
-          deferred.resolve(results);
+        if (remaining === 0) {
+          resolve([]);
         }
-      };
 
-      var rejectAll = function(error) {
-        deferred.reject(error);
-      };
-
-      for (var i = 0; i < promises.length; i++) {
-        if (promises[i] && typeof promises[i].then === 'function') {
-          promises[i].then(resolver(i), rejectAll);
-        } else {
-          resolveAll(i, promises[i]);
+        function resolver(index) {
+          return function(value) {
+            resolveAll(index, value);
+          };
         }
-      }
-      return deferred.promise;
+
+        function resolveAll(index, value) {
+          results[index] = value;
+          if (--remaining === 0) {
+            resolve(results);
+          }
+        }
+
+        for (var i = 0; i < promises.length; i++) {
+          if (promises[i] && typeof promises[i].then === 'function') {
+            promises[i].then(resolver(i), reject);
+          } else {
+            resolveAll(i, promises[i]);
+          }
+        }
+      });
     }
+
 
     __exports__.all = all;
   });
