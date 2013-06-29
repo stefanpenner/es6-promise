dfa0a1566c333de1d8e5d9f75dd645e39a937d9b
Teddy Zeenny <teddyzeenny@gmail.com>
2013-06-29 22:08:23 +0300

    Run reduce tests only in node

--- test/tests/extension_test.js
+++ test/tests/extension_test.js
@@ -979,50 +979,56 @@ describe("RSVP extensions", function() {
   });
 });
 
+
 // thanks to @wizardwerdna for the test case -> https://github.com/tildeio/rsvp.js/issues/66
-describe("using reduce to sum integers using promises", function(){
-  var resolve = RSVP.resolve;
+// Only run these tests in node (phantomjs cannot handle them)
+if (typeof module !== 'undefined' && module.exports) {
 
-  it("should build the promise pipeline without error", function(){
-    var array, iters, pZero, i;
+  describe("using reduce to sum integers using promises", function(){
+    var resolve = RSVP.resolve;
 
-    array = [];
-    iters = 1000;
+    it("should build the promise pipeline without error", function(){
+      var array, iters, pZero, i;
 
-    for (i=1; i<=iters; i++) {
-      array.push(i);
-    }
+      array = [];
+      iters = 1000;
 
-    pZero = resolve(0);
+      for (i=1; i<=iters; i++) {
+        array.push(i);
+      }
 
-    array.reduce(function(promise, nextVal) {
-      return promise.then(function(currentVal) {
-        return resolve(currentVal + nextVal);
+      pZero = resolve(0);
+
+      array.reduce(function(promise, nextVal) {
+        return promise.then(function(currentVal) {
+          return resolve(currentVal + nextVal);
+        });
+      }, pZero);
+    });
+
+    it("should get correct answer without blowing the nextTick stack", function(done){
+      var pZero, array, iters, result, i;
+
+      pZero = resolve(0);
+
+      array = [];
+      iters = 1000;
+
+      for (i=1; i<=iters; i++) {
+        array.push(i);
+      }
+
+      result = array.reduce(function(promise, nextVal) {
+        return promise.then(function(currentVal) {
+          return resolve(currentVal + nextVal);
+        });
+      }, pZero);
+
+      result.then(function(value){
+        assert.equal(value, (iters*(iters+1)/2));
+        done();
       });
-    }, pZero);
-  });
-
-  it("should get correct answer without blowing the nextTick stack", function(done){
-    var pZero, array, iters, result, i;
-
-    pZero = resolve(0);
-
-    array = [];
-    iters = 1000;
-
-    for (i=1; i<=iters; i++) {
-      array.push(i);
-    }
-
-    result = array.reduce(function(promise, nextVal) {
-      return promise.then(function(currentVal) {
-        return resolve(currentVal + nextVal);
-      });
-    }, pZero);
-
-    result.then(function(value){
-      assert.equal(value, (iters*(iters+1)/2));
-      done();
     });
   });
-});
+
+}
