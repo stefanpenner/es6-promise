0347a97df6a28dd3b829b18974dcef6c340dfc6a
Stefan Penner <stefan.penner@gmail.com>
2013-11-22 16:55:53 -0800

    Add Promise#finally

--- test/tests/extension_test.js
+++ test/tests/extension_test.js
@@ -1200,6 +1200,112 @@ describe("RSVP extensions", function() {
       });
     });
   });
+
+  describe("Promise.finally", function() {
+    describe("native finally behaviour", function() {
+      describe("no value is passed in", function() {
+        it("does not provide a value to the finally code", function(done) {
+          var fulfillmentValue = 1;
+          var promise = RSVP.resolve(fulfillmentValue);
+
+          promise['finally'](function() {
+            assert.equal(arguments.length, 0);
+            done();
+          });
+        });
+
+        it("does not provide a reason to the finally code", function(done) {
+          var rejectionReason = new Error();
+          var promise = RSVP.reject(rejectionReason);
+
+          promise['finally'](function(arg) {
+            assert.equal(arguments.length, 0);
+            done();
+          });
+        });
+      });
+
+      describe("non-exceptional cases do not affect the result", function(){
+        it("preserves the original fulfillment value even if the finally callback returns a value", function(done) {
+          var fulfillmentValue = 1;
+          var promise = RSVP.resolve(fulfillmentValue);
+
+          promise['finally'](function() {
+            return 2;
+          }).then(function(value) {
+            assert.equal(fulfillmentValue, value);
+            done();
+          });
+        });
+
+        it("preserves the original rejection reason even if the finally callback returns a value", function(done) {
+          var rejectionReason = new Error();
+          var promise = RSVP.reject(rejectionReason);
+
+          promise['finally'](function() {
+            return 2;
+          }).then(undefined, function(reason) {
+            assert.equal(rejectionReason, reason);
+            done();
+          });
+        });
+      });
+
+      describe("exception cases do propogate the failure", function(){
+        describe("fulfilled promise", function(){
+          it("propagates changes via throw", function(done) {
+            var promise = RSVP.resolve(1);
+            var expectedReason  = new Error();
+
+            promise['finally'](function() {
+              throw expectedReason;
+            }).then(undefined, function(reason) {
+              assert.deepEqual(expectedReason, reason);
+              done();
+            });
+          });
+
+          it("propagates changes via returned rejected promise", function(done){
+            var promise = RSVP.resolve(1);
+            var expectedReason  = new Error();
+
+            promise['finally'](function() {
+              return RSVP.reject(expectedReason);
+            }).then(undefined, function(reason) {
+              assert.deepEqual(expectedReason, reason);
+              done();
+            });
+          });
+        });
+
+        describe("rejected promise", function(){
+          it("propagates changes via throw", function(done) {
+            var promise = RSVP.reject(1);
+            var expectedReason  = new Error();
+
+            promise['finally'](function() {
+              throw expectedReason;
+            }).then(undefined, function(reason) {
+              assert.deepEqual(expectedReason, reason);
+              done();
+            });
+          });
+
+          it("propagates changes via returned rejected promise", function(done){
+            var promise = RSVP.reject(1);
+            var expectedReason  = new Error();
+
+            promise['finally'](function() {
+              return RSVP.reject(expectedReason);
+            }).then(undefined, function(reason) {
+              assert.deepEqual(expectedReason, reason);
+              done();
+            });
+          });
+        });
+      });
+    });
+  });
 });
 
 // thanks to @wizardwerdna for the test case -> https://github.com/tildeio/rsvp.js/issues/66
