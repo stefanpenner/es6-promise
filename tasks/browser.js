c7f692991136916b05bc63eadb139f9cdaa7c5bc
ukyo <ukyo.web@gmail.com>
2014-01-04 14:58:48 +0900

    Web Workers support

--- tasks/browser.js
+++ tasks/browser.js
@@ -1,10 +1,10 @@
 module.exports = function(grunt) {
   grunt.registerMultiTask('browser', 'Export the object in <%= pkg.name %> to the window', function() {
     this.files.forEach(function(f) {
-      var output = ['(function() {'];
+      var output = ['(function(global) {'];
       output.push.apply(output, f.src.map(grunt.file.read));
       output.push("requireModule('promise/polyfill').polyfill();");
-      output.push('}());');
+      output.push('}(this));');
 
       grunt.file.write(f.dest, grunt.template.process(output.join('\n')));
     });
