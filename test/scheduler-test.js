3e02aae7908892e050ea3a3d450d9bd540cf290d
Victor Berchet <victor@suumit.com>
2015-05-12 13:22:06 +0200

    Expose asap() and a way to override the scheduling mechanism on Promise

---
+++ test/scheduler-test.js
@@ -0,0 +1,55 @@
+/*global describe, it, assert */
+
+var g = typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : this;
+var Promise = g.adapter.Promise;
+var assert = require('assert');
+
+describe('scheduler', function() {
+  afterEach(function() {
+      // make sure the es6-promise scheduler is restored after each test
+      Promise._setScheduler(void 0);
+  });
+
+  describe('Promise._setScheduler', function() {
+    it('should allow overriding the default scheduling mechanism', function(done) {
+      // Wrapped in a setTimeout() to make sure that the microtask queue is empty
+      // Otherwise we would have len > 2 and the overriden scheduling mechanism would not
+      // be used.
+      // This is required because the test library uses Promise.
+      setTimeout(function() {
+        var microtasks = [];
+        var resolvedWith = null;
+
+        Promise._setScheduler(function(fn) {
+          microtasks.push(fn);
+        });
+
+        Promise.resolve('value').then(function(v) {
+          resolvedWith = v;
+        });
+
+        assert.equal(resolvedWith, null);
+        assert.equal(microtasks.length, 1);
+
+        while (microtasks.length) {
+          microtasks.shift()();
+        }
+
+        assert.equal(resolvedWith, 'value');
+
+        // restore the original scheduler
+        Promise._setScheduler(void 0);
+        done();
+      });
+    });
+  });
+
+  describe('Promise._asap', function() {
+    it('should allow enqueuing microtasks', function(done) {
+      Promise._asap(function(arg) {
+        assert.equal(arg, 'arg');
+        done();
+      }, 'arg');
+    });
+  });
+});
