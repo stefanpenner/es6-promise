7508b85a417084f68c7d3a2873dc927f3dde276b
Stefan Penner <stefan.penner@gmail.com>
2016-09-26 16:57:16 -0700

    v3.3.1 dist

--- dist/es6-promise.js
+++ dist/es6-promise.js
@@ -3,7 +3,7 @@
  * @copyright Copyright (c) 2014 Yehuda Katz, Tom Dale, Stefan Penner and contributors (Conversion to ES6 API by Jake Archibald)
  * @license   Licensed under MIT license
  *            See https://raw.githubusercontent.com/stefanpenner/es6-promise/master/LICENSE
- * @version   3.3.0
+ * @version   3.3.1
  */
 
 (function (global, factory) {
